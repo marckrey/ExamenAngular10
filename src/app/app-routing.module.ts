d34678ec2126f993c0ce61a1ba1f0d07555ff93e
Marco Reyes Bazán <marebarucv@gmail.com>
2021-01-10 05:42:41 -0500

    feat(1e): Crear una página de control de errores 404 para cuando el usuario ingrese una ruta que no existe.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
 
 const routes: Routes = [
@@ -34,6 +35,10 @@ const routes: Routes = [
         loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
       }
     ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ];
 
